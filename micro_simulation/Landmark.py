b4cbe0cf606c7c6b1337768f8b870ddb3ee87d17
duarte_3333 <dsmorais.33@gmail.com>
2024-05-05 00:10:50 +0100

    weights, matrixes e cenas

---
+++ micro_simulation/Landmark.py
@@ -0,0 +1,31 @@
+import math
+import numpy as np
+
+class Landmark:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.id = id
+        
+        #acts as the mean (or expected value) in the probability distribution describing the landmark's position
+        self.mu = np.array([[self.x],[self.y]]) #column vector
+        
+        #smaller value in the covariance matrix implies higher confidence in the position estimate
+        self.sigma = np.eye(2) * 99 #2x2 covariance matrix associated with the landmark
+        
+        #The np.eye is used to create an identity matrix
+        
+        #The matrix is multiplied by 99 because in beginning
+        #there is high uncertainty in the position of the landmark
+
+    def distance_to(self, x, y):
+        return math.sqrt((self.x - x) ** 2 + (self.y - y) ** 2)
+
+    def update(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+        self.x = self.mu[0][0]
+        self.y = self.mu[1][0]
+        
+    def __str__(self):
+        return f"Landmark(x={self.x}, y={self.y})"
--- micro_simulation/aux_1.py
+++ micro_simulation/aux_1.py
@@ -1,11 +1,9 @@
 import numpy as np
-
-def distance(point1, point2):
-    return np.sqrt((point1[0] - point2[0] )**2 + (point1[1] - point2[1])**2)
+from aux_slam import *
 
 def is_too_close(point, landmarks, min_distance):
     for landmark in landmarks:
-        if distance(point[0], landmark) < min_distance:
+        if euclidean_distance(point[0], landmark) < min_distance:
             return True
     return False
 
@@ -13,8 +11,8 @@ def create_landmarks(nr_landmarks, width, height):
     min_distance = 0.2
     landmarks = []
     while len(landmarks) < nr_landmarks:
-        x_y_proposal = np.random.rand(1, 2)
-        x_y_proposal *= np.array([width, height])
+        x_y_proposal = np.random.rand(1, 2) #this returns a random number between 0 and 1
+        x_y_proposal *= np.array([width, height]) 
         if not is_too_close(x_y_proposal, landmarks, min_distance):
             landmarks.append(tuple(x_y_proposal[0]))
     return landmarks
