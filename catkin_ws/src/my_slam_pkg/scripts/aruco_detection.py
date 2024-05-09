6e9eec463110c5c51a8ae4ae518dd2b554edd3da
Dolfas <rodolfo.amorim@tecnico.ulisboa.pt>
2024-05-09 14:03:14 +0100

    changes to Aruco_Detec

--- catkin_ws/src/my_slam_pkg/scripts/aruco_detection.py
+++ catkin_ws/src/my_slam_pkg/scripts/aruco_detection.py
@@ -22,12 +22,14 @@ class ArucoSLAM:
 
         #the tag of our aruco dictionary is 4X4_100
         
-        self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100) 
+        #self.aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100) 
+        self.aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_100)
 
         #Do not change the tag used (cv2.aruco.DICT_4X4_100), only if we change the ArUcos we're using (and thus their tags) 
 
-        self.parameters =  cv2.aruco.DetectorParameters()
-        self.detector = cv2.aruco.ArucoDetector(self.aruco_dict, self.parameters)
+        #self.parameters =  cv2.aruco.DetectorParameters()
+        self.parameters = aruco.DetectorParameters_create()
+        #self.detector = cv2.aruco.ArucoDetector(self.aruco_dict, self.parameters)
         
         self.map = {} 
     
@@ -42,7 +44,11 @@ class ArucoSLAM:
         self.dist_coeffs = dist.astype(float)
         
     def image_callback(self, data):
-
+        marker_length = 0.1  #length of the marker in meters, change this if we use another marker 
+        world_coords = np.array([[0, 0, 0],
+                             [marker_length, 0, 0],
+                             [marker_length, marker_length, 0],
+                             [0, marker_length, 0]], dtype=np.float32)
         def cart2pol(x, y):
             rho = np.sqrt(x**2 + y**2)
             phi = np.arctan2(y, x)
@@ -61,14 +67,25 @@ class ArucoSLAM:
             for i in range(len(ids)):
                 marker_corners = corners[i][0]
                 cv2.polylines(cv_image, [np.int32(marker_corners)], True, (0, 255, 0), 2)  # Bounding Box
-                rvec, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.1, self.camera_matrix, self.dist_coeffs)
-                markPos = -tvec.reshape((1,3))
-                _,phi = cart2pol(tvec[0][0][2],tvec[0][0][0])
-                phi = phi *180 /np.pi            
- 
+                _, tvec, _ = cv2.aruco.estimatePoseSingleMarkers(corners[i], 0.1, self.camera_matrix, self.dist_coeffs)
+                #markPos = tvec.reshape((1,3))
+                #_,phi = cart2pol(tvec[0][0][2],tvec[0][0][0]-0.1)
+                #phi = phi *180 /np.pi            
+
+            
+                homography, _ = cv2.findHomography(world_coords, marker_corners, cv2.RANSAC)
+                essential_matrix = np.dot(np.dot(np.linalg.inv(self.camera_matrix), homography),self.camera_matrix)
+                u, _, _ = np.linalg.svd(essential_matrix)
+
+                translation_vector = u[:, -1]
+                translation_vector /= np.linalg.norm(translation_vector)
+                _,phi = cart2pol(translation_vector[2],translation_vector[0])
+                phi = phi *180 /np.pi
+
+
                 # ID of the marker
-                cv2.putText(cv_image, str(ids[i][0]), (int(marker_corners[0][0] - 10), int(marker_corners[0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
-                cv2.putText(cv_image, str(round(tvec[0][0][2], 5)), (int(marker_corners[2][0] - 80), int(marker_corners[2][1]) + 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+                #cv2.putText(cv_image, str(ids[i][0]), (int(marker_corners[0][0] - 10), int(marker_corners[0][1]) - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 3)
+                cv2.putText(cv_image, str(round(tvec[0][0][2], 3)), (int(marker_corners[2][0] - 80), int(marker_corners[2][1]) + 45), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
                 cv2.putText(cv_image,  'ang='+str(round(phi,3)), (int(marker_corners[1][0]-70),int(marker_corners[1][1])-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255 ), 3)
    
    
